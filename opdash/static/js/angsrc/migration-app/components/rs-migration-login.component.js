87c5fab9226913e5017bc9476b06501f6a54ed38
Jay Baugh <jay.baugh@rackspace.com>
2016-12-05 14:49:33 -0500

    Fixed binding on login

--- opdash/static/js/angsrc/migration-app/components/rs-migration-login.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-migration-login.component.js
@@ -5,15 +5,21 @@
     angular.module("migrationApp")
         .component("rsmigrationlogin", {
             templateUrl: "/static/angtemplates/migration/login.html",
-            controllerAs: "vm",
-            controller: function() {
-              this.username = 'thisisusername';
-              this.password = '';
-              return {
-                login: function() {
-                  console.info('USERNAME:', this.username);
-
-                }
+            bindings: {
+              username: "@",
+              password: "@"
+            },
+            controller: function($scope, $element, $attrs) {
+              this.$onInit = function () {
+                // This sets the default values
+                this.username = '';
+                this.password = '';
+              };
+              this.onSubmit = function() {
+                // validate form here
+                // if valid, submit form to login here
+                console.info('USERNAME:', this.username);
+                console.info('PASSWORD:', this.password);
               }
             }
 
