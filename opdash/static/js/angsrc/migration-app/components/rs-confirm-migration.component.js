feb93d6d465b34d9acd23b5d163be7e01cf8d7c8
Bharat Gupta <bharat7gupta@gmail.com>
2017-02-01 22:11:03 +0530

    confirm and migrate page

--- opdash/static/js/angsrc/migration-app/components/rs-confirm-migration.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-confirm-migration.component.js
@@ -25,25 +25,40 @@
              * @name migrationApp.controller:rsconfirmmigrationCtrl
              * @description Controller to handle all view-model interactions of {@link migrationApp.object:rsconfirmmigration rsconfirmmigration} component
              */
-            controller: [ "$rootRouter","datastoreservice","migrationitemdataservice", "$q","httpwrapper", function($rootRouter,dataStoreService,ds,$q,HttpWrapper) {
+            controller: [ "$rootRouter","datastoreservice","migrationitemdataservice", "$q","httpwrapper", "authservice", function($rootRouter,dataStoreService,ds,$q,HttpWrapper, authservice) {
                 var vm = this;
                 
                 vm.$onInit = function() {
                     $('title')[0].innerHTML =  "Confirm Migration - Rackspace Cloud Migration";
 
-                    vm.allItems = [];
-                    vm.disable = false;
-                    vm.types = ['server','network'];
-                    var dateObject = dataStoreService.returnDate();
-                    var date = dateObject.date !== ''?moment(dateObject.date).format("MMM Do YY"):moment().format("MMM Do YY");
-                    vm.dateString = date+" at "+dateObject.time+" in "+dateObject.timezone;
+                    var auth = authservice.getAuth();
+                    vm.tenant_id = auth.tenant_id;
+                    vm.tenant_name = auth.rackUsername;
+                    vm.userOrTenant = auth.is_racker ? "Tenant" : "User";
+
+                    vm.selectedItems = {};
+                    vm.selectedItems.servers = ["cloud-endure-demo-A_db_slave", "cloud-endure-demo-B_db_slave"];
+                    vm.selectedItems.files = ["File 1", "File 2", "File 3"];
+                    vm.selectedItems.blocks = ["Block 1", "Block 2", "Block 3", "Block 4"];
+                    vm.selectedItems.cdns = ["CDN 1"];
+
+                    vm.destination = "AWS EC2";
+                    vm.scheduledDateTime = "1/11/2017";
+                    vm.cost = 431.85;
+
+                    // vm.allItems = [];
+                    // vm.disable = false;
+                    // vm.types = ['server','network'];
+                    // var dateObject = dataStoreService.returnDate();
+                    // var date = dateObject.date !== ''?moment(dateObject.date).format("MMM Do YY"):moment().format("MMM Do YY");
+                    // vm.dateString = date+" at "+dateObject.time+" in "+dateObject.timezone;
                     
-                    vm.types.map(function(type,index){
-                        var data = dataStoreService.getItems(type);
-                        data.map(function(item){
-                            vm.allItems.push(item);
-                        });
-                    });
+                    // vm.types.map(function(type,index){
+                    //     var data = dataStoreService.getItems(type);
+                    //     data.map(function(item){
+                    //         vm.allItems.push(item);
+                    //     });
+                    // });
                 };
 
                 /**
@@ -62,14 +77,11 @@
                     });
                     requestObj = ds.prepareRequest(equipments);
 
-                    console.log("-------------");
                     console.log(requestObj);
-                    console.log("-------------");
-                    HttpWrapper.save("/api/job", {"operation":'POST'}, requestObj).then(function(result){
-                        console.log("results inside component");
-                        console.log(result);
-                        $rootRouter.navigate(["MigrationStatus"]);
-                    })
+                    // HttpWrapper.save("/api/job", {"operation":'POST'}, requestObj).then(function(result){
+                    //     console.log(result);
+                    //     $rootRouter.navigate(["MigrationStatus"]);
+                    // });
                 };
                 return vm;
             }
