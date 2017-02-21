39f729a6ae3b133900a76e6db8ea95c331a3a7df
Chiranjib Nandy <Chiranjib.Nandy@accionlabs.in>
2017-02-21 20:28:01 +0530

    Racker Dash Remove From Panel API Integration Changes

--- opdash/static/js/angsrc/migration-app/components/rs-migration-racker-dash.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-migration-racker-dash.component.js
@@ -21,12 +21,20 @@
              * @name migrationApp.controller:rsmigrationrackerdashCtrl
              * @description Controller to handle all view-model interactions of {@link migrationApp.object:rsmigrationrackerdash rsmigrationrackerdash} component
              */
-            controller:["authservice", "datastoreservice", "$scope","httpwrapper","$q","$rootRouter","$window",function(authservice,datastoreservice, $scope,HttpWrapper,$q,$rootRouter,$window){
+            controller:["authservice", "datastoreservice", "$scope","httpwrapper","$q","$rootRouter","$window","$timeout",function(authservice,datastoreservice, $scope,HttpWrapper,$q,$rootRouter,$window,$timeout){
                 var vm = this;
                 vm.items = [];
                 vm.addedAccount = '';
                 const username = authservice.getAuth().username;
 
+                //gets the tenant info when a racker logs in
+                /**
+                 * @ngdoc method
+                 * @name getTenants
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * Fetches the tenant info list when a racker logs in.
+                 */
                 vm.getTenants = function(){                   
                      return HttpWrapper.send('/api/tenants/get_user_tenants/'+username, {"operation":'GET'})
                     .then(function(result){
@@ -49,7 +57,7 @@
 
                 }
 
-                // Perfoming controller initialization steps
+                //performs controller initialization steps
                 vm.$onInit = function() { 
                     console.log("In racker dash");
                     datastoreservice.resetAll();
@@ -58,6 +66,7 @@
                     vm.loadError = false;
                     vm.loadTenantError = false;
                     vm.saveInProgress = false,
+                    vm.showFetch = false;
                     
                     vm.account_name = null;
                     authservice.getAuth().tenant_id= null;
@@ -86,28 +95,82 @@
                    
                 }
 
-                vm.removeAccount = function(accountName){
-                    		var index = -1;		
-                            var comArr = eval( vm.items );
-                            for( var i = 0; i < comArr.length; i++ ) {
-                                if( comArr[i].accountName === accountName ) {
-                                    index = i;
-                                    break;
-                                }
-                            }
-                            vm.items.splice( index, 1 );	
+                //removes the selected tenant data from the tenant info grid
+                /**
+                 * @ngdoc method
+                 * @name removeTenantAccount
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * removes the selected tenant data from the tenant info grid.
+                 */
+                vm.removeTenantAccount = function(item){
+                    vm.loading = true;
+                    vm.loadError = false;
+
+                    var tenant_id_text = item.accountName;
+                    var tenant_id = tenant_id_text.substring(tenant_id_text.lastIndexOf("#")+1,tenant_id_text.lastIndexOf(")"));
+
+                    HttpWrapper.delete("/api/tenants/remove_user_tenant/"+username+"/"+tenant_id, {"operation":'delete'})
+                    .then(function(result){
+                        vm.loading = false;
+                        vm.items =[];
+                        vm.showFetch = true;
+                        vm.fetchResponse = "Successfully removed tenant id";
+
+                        result.forEach(function(item){
+                            vm.items.push({
+                                "accountName":item.rax_name+" (#"+item.tenant_id+")",
+                                "serviceLevel":item.rax_service_level,
+                                "accountLevel":item.faws_service_level,
+                                "inProgressBatches":item.migrations_in_progress,
+                                "completedBatches":item.migrations_completed,
+                                "tenant_account_name":item.rax_name
+                            });
+                        });
+
+                        $timeout(function () {
+                            vm.showFetch = false;
+                            vm.fetchResponse = "";
+                        }, 4000); 
+
+                    }).catch(function(error){
+                        vm.loading = false;
+                        vm.loadError = true;
+                        vm.showFetch = false;
+                        vm.fetchResponse = "";
+                        console.log("Error in removing tenant id");
+                    });
+
                 }
 
+                //opens add tenant id modal 
+                /**
+                 * @ngdoc method
+                 * @name openAddAccountModalDialog
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * opens a modal to add new tenant id.
+                 */
                 vm.openAddAccountModalDialog = function(){
                     vm.loadTenantError = false;
                     $('#add_account_modal').modal('show');
                     vm.addedAccount = '';
                 }
 
+                //fetches the tenant info for the newly entered tenant id
+                /**
+                 * @ngdoc method
+                 * @name submitAddAccount
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * fetches the tenant info for the newly entered tenant id.
+                 */
                 vm.submitAddAccount = function() { 
                     vm.loading = true;
                     vm.loadError = false;
                     vm.loadTenantError = false;
+                    vm.showFetch = true;
+                    vm.fetchResponse = "Successfully added new tenant id";
 
                     vm.saveInProgress = true;
 
@@ -117,8 +180,6 @@
                         "tenant_id":tenant_id,
                         "user_id":username
                     }
-
-                    console.log("requestObj",requestObj);
 
                     HttpWrapper.save("/api/tenants/add_user_tenant", {"operation":'POST'}, requestObj)
                     .then(function(result){
@@ -138,10 +199,16 @@
                                 "tenant_account_name":item.rax_name
                             });
                         });
+                        $timeout(function () {
+                            vm.showFetch = false;
+                            vm.fetchResponse = "";
+                        }, 4000); 
+
                     }).catch(function(error){
                         vm.loading = false;
                         vm.loadTenantError = true; 
-                        
+                        vm.showFetch = false;
+                        vm.fetchResponse = "";                       
                         vm.saveInProgress = false;  
                         console.log("Error in saving tenant id");
                     });
@@ -149,6 +216,14 @@
                     $('#save_for_later').modal('show');
                 }
 
+                //sets the tenant id and redirects the racker to the MigrationResourceList page
+                /**
+                 * @ngdoc method
+                 * @name setTenantId
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * sets the tenant id and redirects the racker to the MigrationResourceList page.
+                 */
                 vm.setTenantId = function(item){
                     var tenant_id_text = item.accountName;
                     var tenant_id = tenant_id_text.substring(tenant_id_text.lastIndexOf("#")+1,tenant_id_text.lastIndexOf(")"));
@@ -157,6 +232,14 @@
                     $rootRouter.navigate(["MigrationResourceList"]);
                 } 
 
+                //sets the tenant id and redirects the racker to the MigrationStatus page
+                /**
+                 * @ngdoc method
+                 * @name setMigrationStatusTenantId
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * sets the tenant id and redirects the racker to the MigrationStatus page.
+                 */
                 vm.setMigrationStatusTenantId = function(item){
                     var tenant_id_text = item.accountName;
                     var tenant_id = tenant_id_text.substring(tenant_id_text.lastIndexOf("#")+1,tenant_id_text.lastIndexOf(")"));
@@ -165,6 +248,14 @@
                     $rootRouter.navigate(["MigrationStatus"]);
                 } 
 
+                //sets the tenant id and opens the encore link in a new browser tab
+                /**
+                 * @ngdoc method
+                 * @name setEncoreLink
+                 * @methodOf migrationApp.controller:rsmigrationrackerdashCtrl
+                 * @description 
+                 * sets the tenant id and opens the encore link in a new browser tab.
+                 */
                 vm.setEncoreLink = function(item){
                     var tenant_id_text = item.accountName;
                     var tenant_id = tenant_id_text.substring(tenant_id_text.lastIndexOf("#")+1,tenant_id_text.lastIndexOf(")"));
