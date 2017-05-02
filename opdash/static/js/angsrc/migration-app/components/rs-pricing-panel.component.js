683a7aeefc572a2e93eb43bd0721637f2c0fbb6d
Chiranjib <chiranjib@l7nk5k32.rackspace.corp>
2017-05-02 18:54:58 +0530

    Pricing Panel and Recommendation Item Template Changes for NA Servers

--- opdash/static/js/angsrc/migration-app/components/rs-pricing-panel.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-pricing-panel.component.js
@@ -363,6 +363,7 @@
                                 "rax_bandwidth_cost":"NA",
                                 "rax_bandwidth":"NA",
                                 "rax_uptime":"NA",
+                                "rax_computed_total_cost":server.details.rax_price,
                                 "rax_total_cost":"NA"
                             });
                             vm.totalOfCostCalculationItems += server.details.rax_price;
@@ -405,6 +406,7 @@
                                 "aws_bandwidth":"NA",
                                 "aws_uptime":"NA",
                                 "aws_total_cost":parseFloat(parseFloat(parseFloat(cost) * parseFloat(24*30)) + parseFloat("0.10")).toFixed(2),
+                                "aws_computed_uptime_cost":parseFloat(parseFloat(cost) * parseFloat(24*30)).toFixed(2),
                                 "rax_bandwidth":"NA",
                                 "rax_uptime":"NA",
                             });
