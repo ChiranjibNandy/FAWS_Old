d70737d24495c03573118fe040de4e621a32d4a3
Ganesh <ganesh@ALIPL0378.local>
2017-06-05 19:37:34 +0530

    minor html changes in FAWS dropdown

--- opdash/static/js/angsrc/migration-app/components/rs-tenant-faws-details.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-tenant-faws-details.component.js
@@ -69,7 +69,7 @@
                     vm.showCreateAccount = false;
 
                 if (vm.currentPage == "ConfirmMigration"){
-                    vm.displayFawsDropdown = false;
+                     vm.displayFawsDropdown = false;
                 }
                 else{
                     vm.displayFawsDropdown = true;
@@ -95,12 +95,12 @@
                                 if(dataStoreService.fetchFawsDetails().selectedFawsAccount == undefined || dataStoreService.fetchFawsDetails().selectedFawsAccount == ''){
                                     vm.selectedFawsName = vm.awsAccountsDetails[0].name;
                                     vm.selectedFawsNum=vm.awsAccountsDetails[0].awsAccountNumber;
-                                    vm.selectedFaws = vm.selectedFawsName + "(#" +  vm.selectedFawsNum + ")"; //to display in faws dropdown default value 
+                                    vm.selectedFaws = vm.selectedFawsName.trim() + " " + "(#" + vm.selectedFawsNum + ")"; //to display in faws dropdown default value
                                 }
                                 else{
                                     vm.selectedFawsName = dataStoreService.fetchFawsDetails().selectedFawsAccount;
                                     vm.selectedFawsNum = dataStoreService.fetchFawsDetails().selectedFawsAccountNumber;
-                                    vm.selectedFaws = vm.selectedFawsName + "(#" +  vm.selectedFawsNum + ")"; //to display in faws dropdown default value
+                                    vm.selectedFaws = vm.selectedFawsName.trim() + " " + "(#" + vm.selectedFawsNum + ")"; //to display in faws dropdown default value
                                 }
                                 vm.fawsAccountDetails = {
                                         awsAccounts:vm.awsAccountsDetails,
@@ -121,7 +121,7 @@
                     vm.awsAccountsDetails = vm.fawsAccountDetails.awsAccounts;
                     vm.selectedFawsName = vm.fawsAccountDetails.selectedFawsAccount;
                     vm.selectedFawsNum = vm.fawsAccountDetails.selectedFawsAccountNumber;
-                    vm.selectedFaws = vm.selectedFawsName + "(#" +  vm.selectedFawsNum + ")"; //to display in faws dropdown default value
+                    vm.selectedFaws = vm.selectedFawsName.trim() + " " + "(#" + vm.selectedFawsNum + ")"; //to display in faws dropdown default value
                 }
 
                 /**
@@ -140,7 +140,7 @@
                     var tempNum = temp1.split(')')[0];
 
                     vm.selectedFawsName = tempName; //local component vars
-                    vm.selectedFawsNum = tempNum;                                  
+                    vm.selectedFawsNum = tempNum;                          
 
                     vm.fawsAccountDetails.selectedFawsAccount = tempName; //for localstorage save
                     vm.fawsAccountDetails.selectedFawsAccountNumber = tempNum;
