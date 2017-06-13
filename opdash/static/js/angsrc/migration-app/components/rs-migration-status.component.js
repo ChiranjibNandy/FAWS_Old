1d71a1e2ac1ca2e8bdde9ec01bb9a785e05d311e
prerana <prerana.pant@rackspace.com>
2017-06-13 14:42:11 +0530

    code changes

--- opdash/static/js/angsrc/migration-app/components/rs-migration-status.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-migration-status.component.js
@@ -30,7 +30,7 @@
                  * @name migrationApp.controller:rsmigrationstatusCtrl
                  * @description Controller to handle all view-model interactions of {@link migrationApp.object:rsmigrationstatus rsmigrationstatus} component
                  */
-                controller: ["httpwrapper", "datastoreservice", "$rootRouter", "authservice", "dashboardservice", "migrationitemdataservice", "alertsservice", "$filter", "$interval", "$timeout", "$scope", "$route","$window","migrationService","$rootScope", function(HttpWrapper, dataStoreService, $rootRouter, authservice, dashboardService, ds, alertsService, $filter, $interval, $timeout, $scope, $route,$window,migrationService,$rootScope) {
+                controller: ["httpwrapper", "datastoreservice", "$rootRouter", "authservice", "dashboardservice", "migrationitemdataservice", "alertsservice", "$filter", "$interval", "$timeout", "$scope", "$route","$window","migrationService", function(HttpWrapper, dataStoreService, $rootRouter, authservice, dashboardService, ds, alertsService, $filter, $interval, $timeout, $scope, $route,$window,migrationService) {
                     var vm = this, 
                         jobList = [],
                         lastRefreshIntervalPromise,
