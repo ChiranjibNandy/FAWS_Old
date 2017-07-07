40dfbc96e778c350046fbd06b01c69aa246fdaef
Harpreet Singh <Harpreet.Singh@rackspace.com>
2017-07-07 09:51:55 -0500

    Removed unused code
    Removed unused code

--- opdash/static/js/angsrc/migration-app/components/rs-migration-resources-list.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-migration-resources-list.component.js
@@ -478,15 +478,7 @@
                     var tenant_id = authservice.getAuth().tenant_id;
                     vm.fawsLink = "https://mycloud.rackspace.com/cloud/"+tenant_id+"/tickets#new";
                 }
-
-                vm.callTemp= function(){
-                    // var url = "/api/cdn/services";
-                    var url = "/api/cbs/volumes";
-                    HttpWrapper.send(url, { "operation": 'GET' })
-                    .then(function (response) {
-                        console.log(response);
-                    });
-                };
+                
                 return vm;
             }
         ]}); // end of component rsmigrationresourceslist
