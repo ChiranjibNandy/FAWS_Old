d289ca2a99bc0f063359c060333b75e6960bfb5c
Jay Baugh <jay.baugh@rackspace.com>
2017-08-09 17:46:13 -0400

    Added robots.txt file Added favicon.ico file

--- opdash/controllers/unsecure.py
+++ opdash/controllers/unsecure.py
@@ -1,3 +1,5 @@
+import os
+from flask import current_app, send_from_directory
 from opdash.controllers.base import UnsecureBlueprint
 
 
@@ -10,3 +12,19 @@ def health():
         Return health check
     """
     return "OK", 200
+
+
+@mod.route('/robots.txt')
+def robots():
+    # For Google Search Engine
+    return send_from_directory(
+        os.path.join(current_app.root_path, 'static'),
+        'robots.txt', mimetype='text/plain')
+
+
+@mod.route('/favicon.ico')
+def favicon():
+    # Rackspace Icon for Favorites
+    return send_from_directory(
+        os.path.join(current_app.root_path, 'static'),
+        'favicon.ico', mimetype='image/vnd.microsoft.icon')
