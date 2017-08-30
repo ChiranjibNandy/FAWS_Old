3319d8d58eb27fbfa9fb38db4cc03060bbbce694
harshavardhan.p <harshavardhan.p@rackspace.com>
2017-08-30 19:51:38 +0530

    FMD-553 Change in the logic for cancelling a migration

--- opdash/static/js/angsrc/migration-app/components/rs-migration-status.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-migration-status.component.js
@@ -262,10 +262,11 @@
                  */
                 vm.getBatches = function (refresh) {
                     var self = this;
-                    vm.loading = true;
+                    vm.manualRefresh = true;
                     dashboardService.getBatches(refresh)
                     .then(function (response) {
                     if (response && response.error == undefined){
+                        vm.manuallyLoadingAllBatches(false);
                         var validCurrentBatchStatus = ["started", "error", "in progress", "scheduled", "paused"];
                         var validCompletedBatchStatus = ["done","canceled"];
                         jobList = response.jobs.job_status_list;
@@ -360,6 +361,7 @@
                         });
                     }}, function (errorResponse) {
                         vm.loading = false;
+                        vm.manualRefresh = false;
                         vm.currentBatches.loadError = true;
                         vm.completedBatches.loadError = true;
                     });
@@ -367,11 +369,27 @@
                         vm.autoRefreshEveryMinute = true;
                         vm.intervalPromise = $interval(function () {   
                             if(dataStoreService.getPageName() === 'MigrationStatus' && vm.autoRefreshText === "ON"){
+                                vm.manuallyLoadingAllBatches(true);
                                 vm.getBatches(true);
                                 vm.onTimeout();
                             }                 
                         }, 60000);
                     }        
+                };
+
+                /**
+                 * @ngdoc method
+                 * @name manuallyLoadingAllBatches
+                 * @methodOf migrationApp.controller:rsmigrationstatusCtrl
+                 * @param {Boolean} 
+                 * @description 
+                 * This function helps to manually refresh the batches without the concern of API's.
+                 */
+                vm.manuallyLoadingAllBatches = function(flag){
+                    var batches = vm.currentBatches.items;
+                    for(var i = 0;i<vm.currentBatches.items.length;i++){
+                        batches[i].showRefreshForApiLoading = flag;
+                    }
                 };
 
                 /**
@@ -392,6 +410,7 @@
                        vm.autoRefreshButton = false;
                        vm.onTimeout();
                        vm.counter = 60;
+                       vm.manuallyLoadingAllBatches(true);
                        vm.getBatches(true);
                     }
                     dashboardService.storeAutoRefreshStatus(vm.autoRefreshText);
@@ -664,6 +683,8 @@
                                 if(detail === 'cancel'){
                                     for(var i = 0;i<vm.currentBatches.items.length;i++){
                                         if(batch.job_id === vm.currentBatches.items[i].job_id){
+                                            vm.currentBatches.items[i].batch_status = 'canceled';
+                                            vm.completedBatches.items.push(vm.currentBatches.items[i]);
                                             vm.currentBatches.items.splice(i,1);
                                             return;
                                         }
