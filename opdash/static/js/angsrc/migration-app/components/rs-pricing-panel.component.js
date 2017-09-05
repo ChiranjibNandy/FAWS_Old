f538ef3b84f179642c5c992efb305c6f64d46690
harshavardhan.p <harshavardhan.p@rackspace.com>
2017-09-05 20:38:16 +0530

    FMD-560 Showing failure description on prereq's popup

--- opdash/static/js/angsrc/migration-app/components/rs-pricing-panel.component.js
+++ opdash/static/js/angsrc/migration-app/components/rs-pricing-panel.component.js
@@ -103,17 +103,17 @@
                                             if(subObj.type=="error"){
                                                 vm.errors.push({
                                                     name:server.name,
-                                                    description:server.description
+                                                    description:subObj.description
                                                 })
                                             }else if(subObj.type=="warning"){
                                                 vm.warnings.push({
                                                     name:server.name,
-                                                    description:server.description
+                                                    description:subObj.description
                                                 })
                                             }else if(subObj.type=="failure"){
                                                 vm.failures.push({
                                                     name:server.name,
-                                                    description:server.description
+                                                    description:subObj.description
                                                 })
                                             }
                                         });
